07e1cf0c85ed225f6b41f06bc8a341d047082942
Benson Wong <mostlygeek+git@gmail.com>
2018-03-23 18:34:30 -0700

    make pool more concurrency safe

--- pool.go
+++ pool.go
@@ -1,22 +1,38 @@
 package exiftool
 
+import (
+	"errors"
+	"sync"
+)
+
 // Pool creates multiple stay open exiftool instances and spreads the work
 // across them with a simple round robin distribution.
 type Pool struct {
+	sync.Mutex
 	stayopens []*Stayopen
 	c         int
 	l         int
+	stopped   bool
 }
 
 func (p *Pool) Extract(filename string) (*Metadata, error) {
+	if p.stopped {
+		return nil, errors.New("Stopped")
+	}
+	p.Lock()
 	p.c++
-	return p.stayopens[p.c%p.l].Extract(filename)
+	key := p.c % p.l
+	p.Unlock()
+	return p.stayopens[key].Extract(filename)
 }
 
 func (p *Pool) Stop() {
+	p.Lock()
+	defer p.Unlock()
 	for _, s := range p.stayopens {
 		s.Stop()
 	}
+	p.stopped = true
 }
 
 func NewPool(exiftool string, num int) *Pool {
