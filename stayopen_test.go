08f6df6ad733ab417e4f000682463d03ee0fcb4f
Benson Wong <mostlygeek@gmail.com>
2018-03-25 17:56:49 -0700

    fix bugs in stayopen scanner (#6)

--- stayopen_test.go
+++ stayopen_test.go
@@ -1,6 +1,8 @@
 package exiftool
 
 import (
+	"bufio"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,4 +35,60 @@ func TestStayOpen(t *testing.T) {
 func TestStayOpenErrorsOnBadBin(t *testing.T) {
 	_, err := NewStayopen("not.a.rea.bin")
 	assert.Error(t, err)
+}
+
+func TestSplitReadyToken(t *testing.T) {
+	assert := assert.New(t)
+	data := []byte("xxx\n{ready}\nyyy\n{ready}\nzzz\n{ready}\n")
+
+	advance, token, err := splitReadyToken(data, false)
+	assert.NoError(err)
+	assert.Equal(12, advance)
+	assert.Equal([]byte("xxx"), token)
+
+	data = data[advance:]
+	advance, token, err = splitReadyToken(data, false)
+	assert.NoError(err)
+	assert.Equal(12, advance)
+	assert.Equal([]byte("yyy"), token)
+
+	data = data[advance:]
+	advance, token, err = splitReadyToken(data, true)
+	assert.Equal(bufio.ErrFinalToken, err)
+	assert.Equal(12, advance)
+	assert.Equal([]byte("zzz"), token)
+}
+
+// TestSplitReadyTokenPartial tests that more data is requested
+// when we don't have a full delimter yet
+func TestSplitReadyTokenPartial(t *testing.T) {
+	assert := assert.New(t)
+	data := []byte("xxx\n{ready}")                      // missing \n
+	advance, token, err := splitReadyToken(data, false) // not at EOF
+	assert.Equal(0, advance)
+	assert.Nil(token)
+	assert.Nil(err)
+}
+
+// TestSplitReadyToken tests behaviour when we've hit
+// EOF on a Reader but we don't have a full delimiter yet
+func TestSplitReadyTokenEOF(t *testing.T) {
+	assert := assert.New(t)
+	data := []byte("xxx\n{ready") // no full token
+	advance, token, err := splitReadyToken(data, true)
+
+	// we should get an io.EOF error and get back all
+	// the data that couldn't be parsed
+	assert.Equal(0, advance)
+	assert.Equal(io.EOF, err)
+	assert.Equal(data, token)
+}
+
+func TestSplitReadyTokenFinalToken(t *testing.T) {
+	assert := assert.New(t)
+	data := []byte("--\n{ready}\n") // just a ready token
+	advance, token, err := splitReadyToken(data, true)
+	assert.Equal(11, advance)
+	assert.Equal([]byte("--"), token)
+	assert.Equal(bufio.ErrFinalToken, err)
 }
